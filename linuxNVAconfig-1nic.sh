4e3ecfbe3813fc67d030c33df273b970a9eec735
erjosito <erjosito@hotmail.com>
2017-04-07 09:49:00 +0200

    1nic shell script included

---
+++ linuxNVAconfig-1nic.sh
@@ -0,0 +1,36 @@
+# These actions will be run at provisioning time
+# Most of these commands are ephemeral, so you will probably have to rerun them if you reboot the VM
+
+# Enable IP forwarding
+sudo -i sysctl -w net.ipv4.ip_forward=1
+
+# Enable a listener on port 1138 (for the internal LB, verify with netstat -lntp)
+while true; do nc -lk -p 1138; done &
+# while true; do nc -lk -p 1138; done &    # We should know the IP for each NVA...
+
+# Enable a listener on port 1139 (for the external LB, verify with netstat -lntp)
+while true; do nc -lk -p 1139; done &
+
+# Install Apache and PHP
+sudo apt-get update
+sudo apt-get install apache2 -y
+sudo apt-get install php libapache2-mod-php php-mcrypt php-mysql -y
+sudo systemctl restart apache2
+
+# Delete default web site and download a new one
+sudo rm /var/www/html/index.html
+sudo apt-get install wget -you
+sudo wget https://raw.githubusercontent.com/erjosito/azure-networking-lab/master/index.php -P /var/www/html/
+
+
+###########################
+#  Firewall config rules  #
+###########################
+
+# Deny forwarded ICMP
+sudo iptables -A FORWARD -p icmp -j DROP
+# Allow forwarded traffic
+sudo iptables -A FORWARD -i eth0 -j ACCEPT
+sudo iptables -A FORWARD -o eth0 -j ACCEPT
+# SNAT for all traffic
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
