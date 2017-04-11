08b02225d4e5d7ba79e32c03e663d97716634802
erjosito <erjosito@hotmail.com>
2017-04-11 15:04:50 +0200

    ILB only

--- udr.sh
+++ udr.sh
@@ -1,12 +1,16 @@
 # Variables
 inetIp=188.113.88.193
 defGw=10.4.2.100
+rg=vnetTest
+
+# Set default resource group
+az configure --defaults group=$rg
 
 # Create route tables
-az network route-table create "vnet1-subnet1"
-az network route-table create "vnet2-subnet1"
-az network route-table create "vnet3-subnet1"
-az network route-table create "vnet4-gw"
+az network route-table create --name vnet1-subnet1
+az network route-table create --name vnet2-subnet1
+az network route-table create --name vnet3-subnet1
+az network route-table create --name vnet4-gw
 
 # Create routes in vnet1
 az network route-table route create --address-prefix 10.1.1.0/24 --next-hop-ip-address $defGw --next-hop-type VirtualAppliance --route-table-name vnet1-subnet1 -n subnet1
@@ -33,7 +37,7 @@ az network route-table route create --address-prefix 10.2.0.0/16 --next-hop-ip-a
 az network route-table route create --address-prefix 10.3.0.0/16 --next-hop-ip-address $defGw --next-hop-type VirtualAppliance --route-table-name vnet4-gw -n vnet3
 
 # Associate route tables to subnets
-az network vnet subnet update -n myVnet1Subnet1 --vnet-name myVnet1 --route-table vnet1-subnet1
-az network vnet subnet update -n myVnet2Subnet1 --vnet-name myVnet2 --route-table vnet2-subnet1
-az network vnet subnet update -n myVnet3Subnet1 --vnet-name myVnet3 --route-table vnet3-subnet1
-az network vnet subnet update -n GatewaySubnet --vnet-name myVnet4 --route-table vnet4-gw
+az network vnet subnet update -n myVnet1Subnet1 --vnet-name myVnet1 --route-table vnet1-subnet1
+az network vnet subnet update -n myVnet2Subnet1 --vnet-name myVnet2 --route-table vnet2-subnet1
+az network vnet subnet update -n myVnet3Subnet1 --vnet-name myVnet3 --route-table vnet3-subnet1
+az network vnet subnet update -n GatewaySubnet --vnet-name myVnet4 --route-table vnet4-gw
